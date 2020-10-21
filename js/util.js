236410d2d785593f7342dcd71e5870af74a7a29f
lesi333 <lesi333@mail.ru>
2020-10-21 07:06:31 +0300

    Выполняет задание №8: собрать за 60 секунд (необязательное задание)

--- js/util.js
+++ js/util.js
@@ -1,63 +1,61 @@
 'use strict';
 
-(() => {
-  window.util = {
-    mags: {
-      WIZARD_COAT_COLORS: [
-        `rgb(101, 137, 164)`,
-        `rgb(241, 43, 107)`,
-        `rgb(146, 100, 161)`,
-        `rgb(56, 159, 117)`,
-        `rgb(215, 210, 55)`,
-        `rgb(0, 0, 0)`
-      ],
+window.util = {
+  mags: {
+    WIZARD_COAT_COLORS: [
+      `rgb(101, 137, 164)`,
+      `rgb(241, 43, 107)`,
+      `rgb(146, 100, 161)`,
+      `rgb(56, 159, 117)`,
+      `rgb(215, 210, 55)`,
+      `rgb(0, 0, 0)`
+    ],
 
-      WIZARD_EYES_COLORS: [
-        `black`,
-        `red`,
-        `blue`,
-        `yellow`,
-        `green`
-      ],
+    WIZARD_EYES_COLORS: [
+      `black`,
+      `red`,
+      `blue`,
+      `yellow`,
+      `green`
+    ],
 
-      WIZARD_FIREBALL_COLOR: [
-        `#ee4830`,
-        `#30a8ee`,
-        `#5ce6c0`,
-        `#e848d5`,
-        `#e6e848`
-      ],
+    WIZARD_FIREBALL_COLOR: [
+      `#ee4830`,
+      `#30a8ee`,
+      `#5ce6c0`,
+      `#e848d5`,
+      `#e6e848`
+    ],
 
-      WIZARD_NUMBERS: 4
-    },
+    WIZARD_NUMBERS: 4
+  },
 
-    getRandomElement: (arr) => {
-      return arr[Math.floor(Math.random() * arr.length)];
-    },
+  getRandomElement: (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  },
 
-    getMaxElement: (arr) => {
-      let maxElement = arr[0];
+  getMaxElement: (arr) => {
+    let maxElement = arr[0];
 
-      for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > maxElement) {
-          maxElement = arr[i];
-        }
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
       }
-
-      return maxElement;
-    },
-
-    onShowError: (errorMessage) => {
-      const node = document.createElement(`div`);
-
-      node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
-      node.style.position = `absolute`;
-      node.style.left = 0;
-      node.style.right = 0;
-      node.style.fontSize = `30px`;
-
-      node.textContent = errorMessage;
-      document.body.insertAdjacentElement(`afterbegin`, node);
     }
-  };
-})();
+
+    return maxElement;
+  },
+
+  onShowError: (errorMessage) => {
+    const node = document.createElement(`div`);
+
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  }
+};
--- js/wizard.js
+++ js/wizard.js
@@ -1,58 +1,56 @@
 'use strict';
 
-(() => {
-  const wizardElement = document.querySelector(`.setup-wizard`);
-  const wizardCoatElement = wizardElement.querySelector(`.wizard-coat`);
-  const wizardEyesElement = wizardElement.querySelector(`.wizard-eyes`);
-  const setupWizardFireball = document.querySelector(`.setup-fireball-wrap`);
-  const wizardCoatInput = document.querySelector(`input[name="coat-color"]`);
-  const wizardEyesInput = document.querySelector(`input[name="eyes-color"]`);
-  const wizardFireballInput = document.querySelector(`input[name="fireball-color"]`);
+const wizardElement = document.querySelector(`.setup-wizard`);
+const wizardCoatElement = wizardElement.querySelector(`.wizard-coat`);
+const wizardEyesElement = wizardElement.querySelector(`.wizard-eyes`);
+const setupWizardFireball = document.querySelector(`.setup-fireball-wrap`);
+const wizardCoatInput = document.querySelector(`input[name="coat-color"]`);
+const wizardEyesInput = document.querySelector(`input[name="eyes-color"]`);
+const wizardFireballInput = document.querySelector(`input[name="fireball-color"]`);
 
-  let wizard = {
-    onEyesChange(color) {
-      return color;
-    },
-    onCoatChange(color) {
-      return color;
-    },
-    onFireBallChange(color) {
-      return color;
-    }
-  };
+let wizard = {
+  onEyesChange(color) {
+    return color;
+  },
+  onCoatChange(color) {
+    return color;
+  },
+  onFireBallChange(color) {
+    return color;
+  }
+};
 
-  wizardCoatElement.addEventListener(`click`, () => {
-    const newColor = window.util.getRandomElement(window.util.mags.WIZARD_COAT_COLORS);
-    wizardCoatElement.style.fill = newColor;
-    wizardCoatInput.value = newColor;
-    wizard.onCoatChange(newColor);
-  });
+wizardCoatElement.addEventListener(`click`, () => {
+  const newColor = window.util.getRandomElement(window.util.mags.WIZARD_COAT_COLORS);
+  wizardCoatElement.style.fill = newColor;
+  wizardCoatInput.value = newColor;
+  wizard.onCoatChange(newColor);
+});
 
-  wizardEyesElement.addEventListener(`click`, () => {
-    const newColor = window.util.getRandomElement(window.util.mags.WIZARD_EYES_COLORS);
-    wizardEyesElement.style.fill = newColor;
-    wizardEyesInput.value = newColor;
-    wizard.onEyesChange(newColor);
-  });
+wizardEyesElement.addEventListener(`click`, () => {
+  const newColor = window.util.getRandomElement(window.util.mags.WIZARD_EYES_COLORS);
+  wizardEyesElement.style.fill = newColor;
+  wizardEyesInput.value = newColor;
+  wizard.onEyesChange(newColor);
+});
 
-  setupWizardFireball.addEventListener(`click`, () => {
-    const newColor = window.util.getRandomElement(window.util.mags.WIZARD_FIREBALL_COLOR);
-    setupWizardFireball.style.backgroundColor = newColor;
-    wizardFireballInput.value = newColor;
-    wizard.onFireBallChange(newColor);
-  });
+setupWizardFireball.addEventListener(`click`, () => {
+  const newColor = window.util.getRandomElement(window.util.mags.WIZARD_FIREBALL_COLOR);
+  setupWizardFireball.style.backgroundColor = newColor;
+  wizardFireballInput.value = newColor;
+  wizard.onFireBallChange(newColor);
+});
 
-  window.wizard = {
-    setCoatChangeHandler: (cb) => {
-      wizard.onCoatChange = cb;
-    },
+window.wizard = {
+  setCoatChangeHandler: (cb) => {
+    wizard.onCoatChange = cb;
+  },
 
-    setEyesChangeHandler: (cb) => {
-      wizard.onEyesChange = cb;
-    },
+  setEyesChangeHandler: (cb) => {
+    wizard.onEyesChange = cb;
+  },
 
-    setFireBallChangeHandler: (cb) => {
-      wizard.onFireBallChange = cb;
-    }
-  };
-})();
+  setFireBallChangeHandler: (cb) => {
+    wizard.onFireBallChange = cb;
+  }
+};
